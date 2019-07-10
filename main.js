31fef74e037968885aab192bdfe923410ff8e280
creationyun <creationyun@gmail.com>
2019-07-10 06:16:41 +0000

    so basic fake menu order

--- main.js
+++ main.js
@@ -8,48 +8,30 @@ const apiRouter = express.Router();
 app.use(logger('dev', {}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+	extended: true
 }));
 
 app.use('/api', apiRouter);
 
-apiRouter.post('/sayHello', function(req, res) {
-  const responseBody = {
-    version: "2.0",
-    template: {
-      outputs: [
-        {
-          simpleText: {
-            text: "hello I'm Ryan"
-          }
-        }
-      ]
-    }
-  };
-
-  res.status(200).send(responseBody);
-});
-
-apiRouter.post('/showHello', function(req, res) {
-  console.log(req.body);
-
-  const responseBody = {
-    version: "2.0",
-    template: {
-      outputs: [
-        {
-          simpleImage: {
-            imageUrl: "https://t1.daumcdn.net/friends/prod/category/M001_friends_ryan2.jpg",
-            altText: "hello I'm Ryan"
-          }
-        }
-      ]
-    }
-  };
-
-  res.status(200).send(responseBody);
+apiRouter.post('/menuOrder', function(req, res) {
+	var params = req.body.action.params;
+	
+	const responseBody = {
+		version: "2.0",
+		template: {
+			outputs: [
+				{
+					simpleText: {
+						text: params.menu + " " + params.num + "판 주문 완료"
+					}
+				}
+			]
+		}
+	};
+	
+	res.status(200).send(responseBody);
 });
 
 app.listen(3000, function() {
-  console.log('Example skill server listening on port 3000!');
+	console.log('Example skill server listening on port 3000!');
 });
